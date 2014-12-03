ce3922a634d751514d1e22c10a1f04c7e5b26711
Dominique Delor <dominique@lehub-agence.com>
2014-12-03 14:33:49 +0100

    Add project

---
+++ www/js/app.js
@@ -0,0 +1,42 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+angular.module('starter', ['ionic'])
+
+.run(function($ionicPlatform) {
+    $ionicPlatform.ready(function() {
+        // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+        // for form inputs)
+        if(window.cordova && window.cordova.plugins.Keyboard) {
+            cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+        }
+        if(window.StatusBar) {
+            StatusBar.styleDefault();
+        }
+    });
+})
+
+.controller('photoCtrl', function($scope) {
+    console.log('photoCtrl on');
+
+    $scope.merge = function() {
+        var img1 = document.getElementById('img1');
+        var frame = document.getElementById('frame');
+        // var frame = document.getElementsByClassName('active')[0].outerHTML;
+        console.log(img1);
+        // console.log(frames);
+        console.log(frame);
+        var canvas = document.getElementById('myCanvas');
+        var context = canvas.getContext('2d');
+
+        canvas.width = img1.width;
+        canvas.height = img1.height;
+
+        context.globalAlpha = 1.0;
+        context.drawImage(img1, 0, 0);
+        context.globalAlpha = 1.0;
+        context.drawImage(frame, 0, 0);
+    }
+})
